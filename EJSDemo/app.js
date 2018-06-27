065b02bfcd095a337605d711fcb160792d33658f
Galen Casstevens <galencasstevens@gmail.com>
2018-06-27 03:17:35 +0000

    initial commit

---
+++ EJSDemo/app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+   res.render("home"); 
+});
+
+app.get("/fellinlovewith/:thing", function(req, res) {
+    var thing = req.params.thing;
+    res.render("love", {thingVar: thing});
+});
+
+app.get("/posts", function(req, res) {
+   var posts = [
+        {title: "Post 1", author: "Jacob"},   
+        {title: "Post 2", author: "Henry"},   
+        {title: "Post 3", author: "Brian"}   
+    ];
+    
+    res.render("posts", {posts: posts});
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Server is listening...");
+});
